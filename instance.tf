202e76e32471871d1d4dc9bd024dc23cba7f3168
Sujagi02 <sujagi2000@gmail.com>
2023-03-05 17:50:06 +0530

    update changes

--- instance.tf
+++ instance.tf
@@ -16,14 +16,12 @@ resource "aws_instance" "weather-api-app" {
   user_data = <<-EOF
   #!/bin/bash
   sudo yum -y update
-  sudo yum -y install ruby
-  sudo yum -y install wget
-  cd /home/ec2-user
-  wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
-  sudo chmod +x ./install
-  sudo ./install auto
-  sudo yum install -y python-pip
-  sudo pip install awscli
+  sudo yum -y install httpd
+  sudo yum install docker
+  sudo systemctl enable httpd
+  sudo systemctl start httpd
+  sudo systemctl enable docker.service
+  sudo systemctl start docker.service
   EOF
 
   tags = {
