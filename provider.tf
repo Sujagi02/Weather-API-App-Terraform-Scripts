58ba9aafe3d8cb1b439be93e5d4d14e49029bc3c
Sujagi02 <sujagi2000@gmail.com>
2023-03-06 20:14:05 +0530

    update tfstate file

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "ap-south-1"
+  profile = "Sujagi"
+}
+
+# store the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket  = "weather-api-app-state-bucket"
+    key     = "terraform.tfstate"
+    region  = "ap-south-1"
+    profile = "Sujagi"
+  }
 }
